2bea594b9fad943ee3c3a04132df0562d1054c79
TYS5629 <yutahimi@ac.cyberhome.ne.jp>
2017-08-21 07:55:16 +0000

    post

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
+  before_action :corrent_user, only: [:destroy]
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -14,9 +15,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'メッセージを削除しました'
+    redirect_back(fallback_location: root_path)
   end
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+  
+  def corrent_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
   end
 end
